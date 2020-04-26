d69aaf43283a9034c093ab85c116014fffd3956f
Jason Hoendervanger <renalpha@hotmail.com>
2020-04-26 09:58:43 +0200

    Update BarcodeScanner.java

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -219,7 +219,7 @@ public class BarcodeScanner extends CordovaPlugin {
                                     } catch (JSONException e) {
                                         Log.d(LOG_TAG, "This should never happen");
                                     }
-                                     Toast toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+                                     Toast toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
 
                                                     toast.setText("FOOBAR");
                                 toast.show();
